0f99b345f665131c42bff2c9b47c1e42f1afe44d
ModPhoenix <ivanchenko.b@gmail.com>
2021-11-08 21:16:22 +0200

    feat: performs the + operation with vector + point and vector + vector

--- src/point.rs
+++ src/point.rs
@@ -1,15 +1,17 @@
-use crate::utils::equal::equal;
+use std::ops::Add;
+
+use crate::{utils::equal::equal, vector::Vector};
 
 #[derive(Debug)]
 pub struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
-    w: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub w: f64,
 }
 
 impl Point {
-    fn new(x: f64, y: f64, z: f64) -> Self {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z, w: 1.0 }
     }
 }
@@ -23,9 +25,22 @@ impl PartialEq for Point {
     }
 }
 
+impl Add<Vector> for Point {
+    type Output = Self;
+
+    fn add(self, other: Vector) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::point::Point;
+    use crate::{point::Point, vector::Vector};
 
     #[test]
     fn correct_create_new_point() {
@@ -45,5 +60,20 @@ mod tests {
     fn point_should_always_have_w_equal_1_0() {
         let point = Point::new(4.3, -4.2, 3.1);
         assert_eq!(point.w, 1.0);
+    }
+
+    #[test]
+    fn adding_point_and_vector() {
+        let point = Point::new(3.0, -2.0, 5.0);
+        let vector = Vector::new(-2.0, 3.0, 1.0);
+        assert_eq!(
+            point + vector,
+            Point {
+                x: 1.0,
+                y: 1.0,
+                z: 6.0,
+                w: 1.0
+            }
+        );
     }
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,16 +1,18 @@
-use crate::utils::equal::equal;
+use std::ops::Add;
+
+use crate::{point::Point, utils::equal::equal};
 
 #[derive(Debug)]
 
 pub struct Vector {
-    x: f64,
-    y: f64,
-    z: f64,
-    w: f64,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+    pub w: f64,
 }
 
 impl Vector {
-    fn new(x: f64, y: f64, z: f64) -> Self {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z, w: 0.0 }
     }
 }
@@ -24,9 +26,35 @@ impl PartialEq for Vector {
     }
 }
 
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl Add<Point> for Vector {
+    type Output = Self;
+
+    fn add(self, other: Point) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::vector::Vector;
+    use crate::{point::Point, vector::Vector};
 
     #[test]
     fn correct_create_new_vector() {
@@ -47,5 +75,35 @@ mod tests {
     fn vector_should_always_have_w_equal_0_0() {
         let vector = Vector::new(4.3, -4.2, 3.1);
         assert_eq!(vector.w, 0.0);
+    }
+
+    #[test]
+    fn adding_two_vectors() {
+        let vector1 = Vector::new(3.0, -2.0, 5.0);
+        let vector2 = Vector::new(-2.0, 3.0, 1.0);
+        assert_eq!(
+            vector1 + vector2,
+            Vector {
+                x: 1.0,
+                y: 1.0,
+                z: 6.0,
+                w: 0.0
+            }
+        );
+    }
+
+    #[test]
+    fn adding_vector_and_point() {
+        let vector = Vector::new(3.0, -2.0, 5.0);
+        let point = Point::new(-2.0, 3.0, 1.0);
+        assert_eq!(
+            vector + point,
+            Vector {
+                x: 1.0,
+                y: 1.0,
+                z: 6.0,
+                w: 1.0
+            }
+        );
     }
 }
