e9b05e3e040ab3bd02cc4a20287a463746edcec3
ModPhoenix <ivanchenko.b@gmail.com>
2021-11-20 23:45:33 +0200

    feat: matrix invertibility

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,8 +1,8 @@
 use std::ops::{Index, IndexMut, Mul};
 
-use crate::tuple::Tuple;
+use crate::{tuple::Tuple, utils::equal::equal};
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct Matrix<const D: usize> {
     data: [[f64; D]; D],
 }
@@ -91,6 +91,25 @@ impl Matrix<4> {
     fn is_invertible(&self) -> bool {
         self.determinant() != 0.
     }
+
+    fn inverse(&self) -> Self {
+        if !self.is_invertible() {
+            panic!()
+        }
+
+        let mut result = Self::new();
+        let determinant = self.determinant();
+
+        for row in 0..4 {
+            for col in 0..4 {
+                let cofactor = self.cofactor(row, col);
+
+                result[col][row] = cofactor / determinant;
+            }
+        }
+
+        result
+    }
 }
 
 impl Matrix<3> {
@@ -146,6 +165,20 @@ impl<const D: usize> Index<usize> for Matrix<D> {
 impl<const D: usize> IndexMut<usize> for Matrix<D> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.data[index]
+    }
+}
+
+impl<const D: usize> PartialEq for Matrix<D> {
+    fn eq(&self, other: &Self) -> bool {
+        for row in 0..D {
+            for col in 0..D {
+                if !equal(self[row][col], other[row][col]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
 
@@ -497,5 +530,93 @@ mod tests {
 
         assert_eq!(a.determinant(), 0.);
         assert!(!a.is_invertible());
+    }
+
+    #[test]
+    fn calculating_the_inverse_of_a_matrix() {
+        #[rustfmt::skip]
+        let a = Matrix::from([
+            [-5.,  2.,  6., -8.],
+            [ 1., -5.,  1.,  8.],
+            [ 7.,  7., -6., -7.],
+            [ 1., -3.,  7.,  4.],
+        ]);
+        let b = a.inverse();
+
+        assert_eq!(a.determinant(), 532.);
+        assert_eq!(a.cofactor(2, 3), -160.);
+        assert_eq!(b[3][2], -160. / 532.);
+        assert_eq!(a.cofactor(3, 2), 105.);
+        assert_eq!(b[2][3], 105. / 532.);
+        #[rustfmt::skip]
+        assert_eq!(b, Matrix::from([
+            [ 0.21805,  0.45113,  0.24060, -0.04511],
+            [-0.80827, -1.45677, -0.44361,  0.52068],
+            [-0.07895, -0.22368, -0.05263,  0.19737],
+            [-0.52256, -0.81391, -0.30075,  0.30639],
+        ]));
+    }
+
+    #[test]
+    fn calculating_the_inverse_of_another_matrix() {
+        #[rustfmt::skip]
+        let a = Matrix::from([
+            [ 8., -5.,  9.,  2.],
+            [ 7.,  5.,  6.,  1.],
+            [-6.,  0.,  9.,  6.],
+            [-3.,  0., -9., -4.],
+        ]);
+        let b = a.inverse();
+
+        #[rustfmt::skip]
+        assert_eq!(b, Matrix::from([
+            [-0.15385, -0.15385, -0.28205, -0.53846],
+            [-0.07692,  0.12308,  0.02564,  0.03077],
+            [ 0.35897,  0.35897,  0.43590,  0.92308],
+            [-0.69231, -0.69231, -0.76923, -1.92308],
+        ]));
+    }
+
+    #[test]
+    fn calculating_the_inverse_of_a_third_matrix() {
+        #[rustfmt::skip]
+        let a = Matrix::from([
+            [ 9.,  3.,  0.,  9.],
+            [-5., -2., -6., -3.],
+            [-4.,  9.,  6.,  4.],
+            [-7.,  6.,  6.,  2.],
+        ]);
+        let b = a.inverse();
+
+        #[rustfmt::skip]
+        assert_eq!(b, Matrix::from([
+            [-0.04074, -0.07778,  0.14444, -0.22222],
+            [-0.07778,  0.03333,  0.36667, -0.33333],
+            [-0.02901, -0.14630, -0.10926,  0.12963],
+            [ 0.17778,  0.06667, -0.26667,  0.33333],
+        ]));
+    }
+
+    #[test]
+    fn multiplying_a_product_by_its_inverse() {
+        #[rustfmt::skip]
+        let a = Matrix::from([
+            [ 3., -9.,  7.,  3.],
+            [ 3., -8.,  2., -9.],
+            [-4.,  4.,  4.,  1.],
+            [-6.,  5., -1.,  1.],
+        ]);
+
+        #[rustfmt::skip]
+        let b = Matrix::from([
+            [8.,  2., 2., 2.],
+            [3., -1., 7., 0.],
+            [7.,  0., 5., 4.],
+            [6., -2., 0., 5.],
+        ]);
+
+        let c = a.clone() * b.clone();
+
+        assert_eq!(c * b.inverse(), a);
     }
 }
