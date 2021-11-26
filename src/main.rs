295a10942e2d3f622767c202b1717df815e89a4d
MoDPhoenix <ivanchenko.b@gmail.com>
2021-11-26 22:30:20 +0200

    chore: render silhouette of sphere, drawn to canvas one ray at a time

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,48 @@
+use std::f64::consts::PI;
+use std::fs::File;
 use std::io::prelude::*;
-use std::{f64::consts::PI, fs::File};
 
-use ray_tracer::{canvas::Canvas, color::Color, matrix::Matrix, tuple::Tuple};
-
-#[derive(Debug, Clone)]
-
-struct Projectile {
-    position: Tuple,
-    velocity: Tuple,
-}
-
-#[derive(Debug, Clone)]
-
-struct Environment {
-    gravity: Tuple,
-    wind: Tuple,
-}
-
-fn tick(env: Environment, proj: Projectile) -> Projectile {
-    let position = proj.position.clone() + proj.velocity.clone();
-    let velocity = proj.velocity + env.gravity + env.wind;
-
-    Projectile { position, velocity }
-}
+use ray_tracer::intersections::Intersectable;
+use ray_tracer::matrix::Matrix;
+use ray_tracer::ray::Ray;
+use ray_tracer::sphere::Sphere;
+use ray_tracer::{canvas::Canvas, color::Color, tuple::Tuple};
 
 fn main() -> std::io::Result<()> {
-    let mut canvas = Canvas::new(100, 100);
-    let point_color = Color::new(1.0, 0.0, 1.0);
-    let scaling = Matrix::identity().scaling(25., 25., 25.);
-    let rotation = Matrix::identity().rotation_z(PI / 2.);
-    let twelve = scaling * Tuple::point(1., 1., 0.);
-    let one = rotation * twelve;
-    let two = rotation * one;
-    let tree = rotation * two;
-    let four = rotation * tree;
+    let canvas_pixels = 100;
+    let ray_origin = Tuple::point(0., 0., -5.);
+    let wall_z = 10.;
+    let wall_size = 7.;
+    let pixel_size = wall_size / canvas_pixels as f64;
+    let half = wall_size / 2.;
 
-    canvas.set_center(twelve.x as usize, twelve.y as usize, &point_color);
-    canvas.set_center(one.x as usize, one.y as usize, &point_color);
-    canvas.set_center(two.x as usize, two.y as usize, &point_color);
-    canvas.set_center(tree.x as usize, tree.y as usize, &point_color);
-    canvas.set_center(four.x as usize, four.y as usize, &point_color);
+    let mut canvas = Canvas::new(canvas_pixels, canvas_pixels);
+    let point_color = Color::new(1.0, 0.0, 1.0);
+    let mut shape = Sphere::new();
+
+    shape.set_transform(
+        Matrix::identity()
+            .scaling(0.5, 1., 1.)
+            .shearing(1., 0., 0., 0., 0., 0.),
+    );
+
+    for y in 0..canvas.height {
+        let world_y = half - pixel_size * y as f64;
+        for x in 0..canvas.width {
+            let world_x = -half + pixel_size * x as f64;
+            let position = Tuple::point(world_x, world_y, wall_z);
+
+            let ray = Ray::new(ray_origin, (position - ray_origin).normalize());
+            let xs = shape.intersect(&ray);
+
+            match xs {
+                Some(_) => {
+                    canvas.set(x, y, &point_color);
+                }
+                None => {}
+            }
+        }
+    }
 
     let mut file = File::create("output.ppm")?;
     file.write_all(&canvas.to_ppm().as_bytes())?;
