6359b23a9aa5701e176c3d9deaa4a5422e26dfaa
ModPhoenix <ivanchenko.b@gmail.com>
2021-12-05 02:08:43 +0200

    feat(camera): new method ray_for_pixel

--- src/camera.rs
+++ src/camera.rs
@@ -1,14 +1,14 @@
-use crate::matrix::Matrix;
+use crate::{matrix::Matrix, ray::Ray, tuple::Tuple};
 
 #[derive(Debug)]
 pub struct Camera {
     hsize: usize,
     vsize: usize,
     field_of_view: f64,
-    transform: Matrix<4>,
     half_width: f64,
     half_height: f64,
     pixel_size: f64,
+    transform: Matrix<4>,
 }
 
 impl Camera {
@@ -18,7 +18,6 @@ impl Camera {
 
         let half_width: f64;
         let half_height: f64;
-        let pixel_size: f64;
 
         if aspect >= 1.0 {
             half_width = half_view;
@@ -28,17 +27,38 @@ impl Camera {
             half_height = half_view;
         }
 
-        pixel_size = (half_width * 2.) / hsize as f64;
+        let pixel_size = (half_width * 2.) / hsize as f64;
 
         Self {
             hsize,
             vsize,
             field_of_view,
-            transform: Matrix::identity(),
             half_width,
             half_height,
             pixel_size,
+            transform: Matrix::identity(),
         }
+    }
+
+    pub fn set_transform(mut self, transform: Matrix<4>) -> Self {
+        self.transform = transform;
+        self
+    }
+
+    pub fn ray_for_pixel(&self, px: usize, py: usize) -> Ray {
+        let xoffset = (px as f64 + 0.5) * self.pixel_size;
+        let yoffset = (py as f64 + 0.5) * self.pixel_size;
+
+        let world_x = self.half_width - xoffset;
+        let world_y = self.half_height - yoffset;
+
+        let inverse_transform = self.transform.inverse();
+
+        let pixel = inverse_transform * Tuple::point(world_x, world_y, -1.);
+        let origin = inverse_transform * Tuple::point(0., 0., 0.);
+        let direction = (pixel - origin).normalize();
+
+        Ray::new(origin, direction)
     }
 }
 
@@ -47,6 +67,7 @@ mod tests {
     use std::f64::consts::PI;
 
     use crate::matrix::Matrix;
+    use crate::tuple::Tuple;
     use crate::utils::fuzzy_equal::fuzzy_equal;
 
     use super::Camera;
@@ -77,5 +98,40 @@ mod tests {
         let c = Camera::new(125, 200, PI / 2.);
 
         assert!(fuzzy_equal(c.pixel_size, 0.01));
+    }
+
+    #[test]
+    fn constructing_a_ray_through_the_center_of_the_canvas() {
+        let c = Camera::new(201, 101, PI / 2.);
+        let r = c.ray_for_pixel(100, 50);
+
+        assert_eq!(r.origin, Tuple::point(0., 0., 0.));
+        assert_eq!(r.direction, Tuple::vector(0., 0., -1.));
+    }
+
+    #[test]
+    fn constructing_a_ray_through_a_corner_of_the_canvas() {
+        let c = Camera::new(201, 101, PI / 2.);
+        let r = c.ray_for_pixel(0, 0);
+
+        assert_eq!(r.origin, Tuple::point(0., 0., 0.));
+        assert_eq!(r.direction, Tuple::vector(0.66519, 0.33259, -0.66851));
+    }
+
+    #[test]
+    fn constructing_a_ray_when_the_camera_is_transformed() {
+        let c = Camera::new(201, 101, PI / 2.).set_transform(
+            Matrix::identity()
+                .translation(0., -2., 5.)
+                .rotation_y(PI / 4.),
+        );
+
+        let r = c.ray_for_pixel(100, 50);
+
+        assert_eq!(r.origin, Tuple::point(0., 2., -5.));
+        assert_eq!(
+            r.direction,
+            Tuple::vector(2.0_f64.sqrt() / 2., 0., -2.0_f64.sqrt() / 2.)
+        );
     }
 }
